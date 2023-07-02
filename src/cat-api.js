b09373b8f1a2241fb65c936a3c71fd0a86f17656
Artur <110535829+Primus1C@users.noreply.github.com>
2023-07-02 22:10:47 +0200

    develop

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,8 +4,6 @@ import Notiflix from 'notiflix';
 axios.defaults.headers.common['x-api-key'] =
   'live_YGWRPlgbj8PJkdp9R68mzE5AJ1W7whcwqQBldfENQlehOrKyZKTx11u2KgAJW1oi';
 
-const axios = require('axios');
-
 const baseURL = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {
