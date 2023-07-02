9ef221f9455a815aadfeb77af0ba67bc8820aaf1
Artur Rakhimov <kharkiv1c@gmail.com>
2023-07-02 11:53:23 +0200

    develop

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import SlimSelect from 'slim-select';
 
 const select = document.getElementById('slim');
-console.log(select); 
+console.log(1, select); 
 
 new SlimSelect({
-  select: 'slim-select',
+  select: 'slim',
   //data: [{ text: 'Value 1', value: 'value1' }],
   //onChange: onBreedSelected,
 });
