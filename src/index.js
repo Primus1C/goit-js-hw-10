940343415ed353a94c381aa0abdf7451fe34ecc7
Artur <110535829+Primus1C@users.noreply.github.com>
2023-07-02 11:40:58 +0200

    develop

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import SlimSelect from 'slim-select';
 
-/* let select = document.querySelector('.breed-select');
-console.log(select); */
+const select = document.getElementById('slim');
+console.log(select); 
 
 new SlimSelect({
-  select: '#slim-select',
+  select: 'slim-select',
   //data: [{ text: 'Value 1', value: 'value1' }],
   //onChange: onBreedSelected,
 });
