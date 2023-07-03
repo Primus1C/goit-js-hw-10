098994f8e17fcb0cfe4a9ac89d43d1f5f036ac8d
Artur <110535829+Primus1C@users.noreply.github.com>
2023-07-03 23:35:03 +0200

    develop2

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,4 @@
-import axios from 'axios';
-//import Notiflix from 'notiflix';
-
+/* import axios from 'axios';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_YGWRPlgbj8PJkdp9R68mzE5AJ1W7whcwqQBldfENQlehOrKyZKTx11u2KgAJW1oi';
@@ -22,4 +20,30 @@ export function fetchCatByBreed(breedId) {
       return response.data;
     }));
   return resp;
+} */
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_uPAbgvOj5WRxm08mP9aix5kJU4fWe1265ayGmXdj9j6ShUPbrmokXYHhIKbkxXGw';
+
+function fetchBreeds() {
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
+
+function fetchCatByBreed(breedId) {
+  return fetch(
+    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export { fetchBreeds, fetchCatByBreed };
